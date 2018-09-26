2d399ae1931a635fed04581a8f63b0748ac0086b
10041 <andreyoleksyuk004@gmail.com>
2018-09-26 12:02:11 +0300

    add error processing

--- main.php
+++ main.php
@@ -5,3 +5,49 @@
  * Date: 25.09.18
  * Time: 22:27
  */
+
+$packages = [
+    'A' => [
+        'name' => 'A',
+        'dependencies' => ['B', 'C'],
+    ],
+    'B' => [
+        'name' => 'B',
+        'dependencies' => [],
+    ],
+    'C' => [
+        'name' => 'C',
+        'dependencies' => ['B', 'D'],
+    ],
+    'D' => [
+        'name' => 'D',
+        'dependencies' => [],
+    ],
+];
+
+
+class FormatPackagesException extends \Exception {}
+
+class CycleDependencieException extends \Exception {}
+
+function checkPackagesName(array $packages): boolean{
+
+}
+
+function validatePackageDefinitions(array $packages): void{
+
+}
+
+function getAllPackageDependencies(array $packages, string $packageName): array{
+
+}
+
+try{
+    throw new CycleDependencieException('aaaaaaaa');
+}
+catch (FormatPackagesException $e){
+    print_r($e->getMessage()."\n");
+}
+catch (CycleDependencieException $e){
+    print_r($e->getMessage()."\n");
+}
