aaceaeb1de1dd845c847c4ba21918a907b1076ee
10041 <andreyoleksyuk004@gmail.com>
2018-09-26 14:56:38 +0300

    add checkDep, checkDepKey and php doc

--- main.php
+++ main.php
@@ -6,13 +6,71 @@
  * Time: 22:27
  */
 
+class FormatPackagesException extends \Exception {}
+
+class CycleDependencieException extends \Exception {}
+
+/**
+ * @param array $packages
+ * @throws FormatPackagesException
+ *
+ * @return void
+ */
+function checkPackagesName(array $packages): void{
+    foreach ($packages as $key=>$value){
+        if($key !== $value['name']){
+            throw new FormatPackagesException("the names do not match");
+        }
+    }
+}
+
+/**
+ * @param array $packages
+ * @throws FormatPackagesException
+ *
+ * @return void
+ */
+function checkDependenciesKey(array $packages): void{
+    foreach ($packages as $key=>$value){
+        if(!array_key_exists('dependencies', $value)){
+            throw new FormatPackagesException("not key 'dependencies'");
+        }
+    }
+}
+
+
+/**
+ * @param array $packages
+ * @throws FormatPackagesException
+ *
+ * @return void
+ */
+function checkDependencies(array $packages): void{
+    foreach ($packages as $key=>$value){
+        foreach ($value['dependencies'] as $dependencie){
+            if(!array_key_exists($dependencie, $packages)){
+                throw new FormatPackagesException("dependencie ".$dependencie." not found");
+            }
+        }
+    }
+}
+
+function validatePackageDefinitions(array $packages): void{
+
+}
+
+function getAllPackageDependencies(array $packages, string $packageName): array{
+
+}
+
+
 $packages = [
     'A' => [
         'name' => 'A',
         'dependencies' => ['B', 'C'],
     ],
     'B' => [
-        'name' => 'A',
+        'name' => 'B',
         'dependencies' => [],
     ],
     'C' => [
@@ -25,31 +83,8 @@ $packages = [
     ],
 ];
 
-
-class FormatPackagesException extends \Exception {}
-
-class CycleDependencieException extends \Exception {}
-
-function checkPackagesName(array $packages): bool{
-    foreach ($packages as $key=>$value){
-        if($key !== $value['name']){
-            return FALSE;
-        }
-    }
-    return TRUE;
-}
-
-
-function validatePackageDefinitions(array $packages): void{
-
-}
-
-function getAllPackageDependencies(array $packages, string $packageName): array{
-
-}
-
 try{
-    print_r(checkPackagesName($packages)."\n");
+    print_r(checkDependencies($packages)."\n");
 }
 catch (FormatPackagesException $e){
     print_r($e->getMessage()."\n");
